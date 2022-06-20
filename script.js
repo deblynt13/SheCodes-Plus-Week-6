d9a2684b2bc39bfd6ddf250c0e669f00e5562e47
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-06-20 10:14:52 -0400

    added weather description

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
 
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
 }
