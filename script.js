8f67e37553b2234d4bb0a2bb94c96ce18ef32329
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-06-25 18:00:59 -0400

    updating icon code

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   // iconElement.setAttribute(
   //"src",
