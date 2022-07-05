4cf8ac85f8986d6fa666c43eed66b32e0b26f93f
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-05 12:12:52 -0400

    added to displayForecast

--- script.js
+++ script.js
@@ -57,7 +57,8 @@ forecastElement.innerHTML = forecastHTML;
 function.getForecast(coordinates) {
     let apiKey = "2abcdcc61962b2b7f2b8e768d3b810e8";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units= metric`;
+    axios.get(apiUrl).then(displayForecast);
 
 }
 
