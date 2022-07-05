cbbbd2c3e017816240ddceadda43f413dbf177bb
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-05 17:50:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function displayForecast() {
        <div class = "col-2">
            <div class = "weather-forecast-date">${forecastDay.dt}</div>
            <img
-             src = "https://openweathermap.org/img/wn/${forecast.weather]0].icon}@2x.png"
+             src = "https://openweathermap.org/img/wn/${forecast.weather}[0].icon}@2x.png"
              alt=""
              width="42"
            />   
