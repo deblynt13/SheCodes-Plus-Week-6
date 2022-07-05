fadff11bafaa683f87b98e09fff8b0f356d63819
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-05 18:14:02 -0400

    formatDay function

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ function formatDate(timestamp) {
   let day = days[dayIndex];
   return `${day} ${hours} :${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function displayForecast(response) {
   let forecast = response.data.daily;
@@ -36,7 +43,7 @@ function displayForecast(response) {
       forecastHTML +
       `
        <div class = "col-2">
-           <div class = "weather-forecast-date">${forecastDay.dt}</div>
+           <div class = "weather-forecast-date">${formatDay.dt}</div>
            <img
              src = "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
              alt=""
