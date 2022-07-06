a2b51b232efac8eaf1e2da167adc850182d53805
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-06 18:23:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-       <div class = "col-2">
+       <div class = "col-4">
            <div class = "weather-forecast-date">
              ${formatDay(forecastDay.dt)}</div>
            <img
