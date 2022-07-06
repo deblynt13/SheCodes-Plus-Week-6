b9488d1c770389be7e15757c8b0b9e6d5efbf77c
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-06 12:08:26 -0400

    degree symbol added

--- script.js
+++ script.js
@@ -55,11 +55,11 @@ function displayForecast(response) {
            <div class = "weather-forecast-temperatures">
              <span class = "weather-forecast-temperature-max"> ${Math.round(
                forecastDay.temp.max
-             )} </span> 
+             )} °</span> 
              
-             <span class = "weather-foreceast-temperature-min"> ${Math.round(
+             <span class = "weather-forecast-temperature-min"> ${Math.round(
                forecastDay.temp.min
-             )} </span>
+             )}° </span>
              
            </div>
        </div>
