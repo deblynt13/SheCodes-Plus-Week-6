54c6db51a8c9867888a561991941374fbe4edb89
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-06 17:21:29 -0400

    adjusted API URL to 1 line

--- script.js
+++ script.js
@@ -73,8 +73,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "2abcdcc61962b2b7f2b8e768d3b810e8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-  lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units= metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units= metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
