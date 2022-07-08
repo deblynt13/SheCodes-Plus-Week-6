a2841dde40c06c4b92c877c8c2534c9771560538
deblynt13 <70982424+deblynt13@users.noreply.github.com>
2022-07-08 13:30:19 -0400

    forecast placement

--- script.js
+++ script.js
@@ -42,8 +42,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-       <div class = "col-5">
-          <div class = "row">
+       <div class = "col-2">
            <div class = "weather-forecast-date">
              ${formatDay(forecastDay.dt)}</div>
            <img
